59abdbb3fb9cb8ad39d3949019f5bbba5c562c65
Vishnu <vishnu.garg@oracle.com>
2021-11-13 01:57:15 +0530

    Setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ForgotPasswordComponent } from './main/forgot-password/forgot-password.component';
+import { LoginComponent } from './main/login/login.component';
+import { SignupComponent } from './main/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
